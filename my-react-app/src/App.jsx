f42991b133c7f2e211c05c28aa42e75c15c97bb3
Adesina Joshua <tolujosh1@gmail.com>
2024-06-20 15:31:50 +0200

    first react app completed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,15 @@
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
 
 function App() {
   return(
-    <Header>
-      
-    </Header>
-  )
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+    </>
+  );
 }
 
 export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Adesina Joshua; All Rights Reserved</p>
+        </footer>
+    );
+}
+
+export default Footer
