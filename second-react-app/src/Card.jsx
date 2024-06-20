508d830836f4429b36ea40ccabdcd93567ba9e63
Adesina Joshua <tolujosh1@gmail.com>
2024-06-20 15:48:54 +0200

    building card interface

---
+++ second-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+
+
+function Card(){
+
+    return(
+        <div>
+            <img></img>
+            <h2>Jaytee Omo werey</h2>
+            <p></p>
+            <p>Software Engineer</p>
+        </div>
+    );
+}
+
+export default Card
