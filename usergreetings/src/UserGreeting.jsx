9b4aefc9dcd41b0d11c277edc63867b912fed145
Adesina Joshua <tolujosh1@gmail.com>
2024-07-02 22:53:10 +0200

    react basis contd

---
+++ usergreetings/src/UserGreeting.jsx
@@ -0,0 +1,9 @@
+
+function UserGreeting(props){
+
+    return(props.isLoggedIn ? <h2>welcome {props.username}</h2> :
+                            <h2>Please login to continue</h2>
+    );
+}
+
+export default UserGreeting
